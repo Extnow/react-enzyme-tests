6432c8f448335444638e3c65e72de7fc42348914
extnow <ryabushkin.mikhail@gmail.com>
2019-04-20 09:45:51 +0300

    write test for mount/unmount behavior

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
 
-import { configure, shallow } from 'enzyme';
+import { configure, shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 // testing with styled-components
@@ -9,12 +9,13 @@ import { Header } from './App';
 
 /*
  * shallow - отрисовывает только сам компонент без дочерних
+ * mount - тест mount/unmount лайфсайкл методов
  * configure({ adapter: new Adapter() }); - дефолтная конфигурация enzyme
  */
 
 configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
+describe('<App /> shallow rendering', () => {
   const wrapper = shallow(<App />);
 
   it('should contain 1 p element', () => {
@@ -35,5 +36,15 @@ describe('<App />', () => {
 
   it('tag a should contain text "Learn React"', () => {
     expect(wrapper.find('a').text()).toBe('Learn React');
+  });
+});
+
+describe('<App /> mount rendering', () => {
+  const wrapper = mount(<App />);
+
+  it('h1 contains correct text', () => {
+    expect(wrapper.find('p').length).toBe(1);
+    wrapper.unmount();
+    expect(wrapper.find('p').length).toBe(0);
   });
 });
